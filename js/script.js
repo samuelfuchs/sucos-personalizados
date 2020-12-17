f1c772bd420fa9d98a659ef2061d4c94fcc7ccf8
samuelfuchs <fuchsgerson@gmail.com>
2020-12-17 03:58:44 -0300

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+// navbar
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -9,25 +8,3 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-50px";
   }
 }
-
-// Accordion
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,  
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";  
-    } else {
-      panel.style.display = "block";  
-    }  
-  });  
-}  
-
